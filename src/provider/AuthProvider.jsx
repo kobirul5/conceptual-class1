e355a45d9f0a842bc9e260eab2b680060e139df7
kobirul5 <kobirul05j@gmail.com>
2024-11-13 01:05:12 +0600

    added observer

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,17 +1,18 @@
-import React, { createContext } from 'react';
-import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import React, { createContext, useEffect, useState } from 'react';
+import { GithubAuthProvider, GoogleAuthProvider, onAuthStateChanged, signInWithPopup } from 'firebase/auth';
 import auth from '../firebase/firebase.init';
+import { useActionData } from 'react-router-dom';
 
 export const AuthContext = createContext(null)
 const AuthProvider = ({children}) => {
-
+    const [user, setUser] = useState(null)
     
     const googleProvider = new GoogleAuthProvider()
     const githubProvider = new GithubAuthProvider()
 
     const googleWithLogin = () => {
         signInWithPopup(auth, googleProvider)
-            .then(result => { console.log(result) })
+            .then(result => { setUser(result.user) })
             .catch(error => {
                 console.log("Error", error)
             })
@@ -20,12 +21,19 @@ const AuthProvider = ({children}) => {
     const githubWithLogin = () => {
         signInWithPopup(auth, githubProvider)
         .then(result=>{
-            console.log(result)
+            setUser(result.user)
         })
         .catch(error => {
             console.log("Error", error)
         })
     }
+
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth, currentUser=>{
+            console.log(currentUser)
+        })
+        return unsubscribe()
+    },[user])
 
     const authInfo = {
         googleWithLogin,
